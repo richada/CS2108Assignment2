711760a915af0c078a5ba30d4a02b9271f537530
mengdi zhang <mengdi0901@gmail.com>
2015-10-12 20:49:04 +0800

    implement distance measures and add lib folder

--- src/Distance/CityBlock.java
+++ src/Distance/CityBlock.java
@@ -5,9 +5,49 @@ package Distance;
  */
 public class CityBlock {
     public double getDistance(double[] query1, double[] query2){
-        /**
-         * Please implement the distance/similarity function by yourself.
-         */
-        return 0.0;
+        double distance;
+    	
+    	if (query1.length != query2.length){
+            System.err.println("The dimension of the two vectors does not match!");
+
+            System.exit(1);
+        }
+
+        double distanceSum = 0.0;
+        double range = 0.0;
+        double maximum = 0.0;
+        double minimum = 0.0;
+
+        for (int i = 0; i < query1.length; i++){
+            distanceSum += Math.abs(query1[i]-query2[i]);
+        }
+        for (int i = 0; i < query1.length; i++){
+            if (query1[i] > maximum){
+            	maximum = query1[i];
+            }
+            if (query2[i] > maximum){
+            	maximum = query2[i];
+            }
+        }
+        minimum = maximum;
+        for (int i = 0; i < query1.length; i++){
+            if (query1[i] < minimum){
+            	minimum = query1[i];
+            }
+            if (query2[i] < minimum){
+            	minimum = query2[i];
+            }
+        }
+        
+        range = maximum - minimum;
+        
+        if (range != 0.0){
+        	distance = distanceSum/range/query1.length;
+        }
+        else {
+        	distance = 0.0;
+        }
+
+        return distance;
     }
 }
--- src/Distance/Euclidean.java
+++ src/Distance/Euclidean.java
@@ -6,9 +6,37 @@ package Distance;
 public class Euclidean {
 
     public double getDistance(double[] query1, double[] query2){
-        /**
-         * Please implement the distance/ similarity function by yourselves.
-         */
-        return 0.0;
+    	double distance;
+        
+    	if (query1.length != query2.length){
+            System.err.println("The dimension of the two vectors does not match!");
+
+            System.exit(1);
+        }
+    	double[] difference = new double[query1.length];
+    	for (int i = 0; i < query1.length; i ++){
+    		difference[i] = query1[i] - query2[i];
+    	}
+    	
+    	distance = 0.5 * getVariance(difference)/(getVariance(query1)-getVariance(query2));
+    	return distance;
+    }
+    double getMean(double[] array)
+    {
+        double sum = 0.0;
+        int size = array.length;
+        for(double a : array)
+            sum += a;
+        return sum/size;
+    }
+
+    double getVariance(double[] array)
+    {
+        double mean = getMean(array);
+        double temp = 0;
+        int size = array.length;
+        for(double a :array)
+            temp += (mean-a)*(mean-a);
+        return temp/size;
     }
 }
